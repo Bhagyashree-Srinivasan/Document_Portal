ea0892af624f361f54b1c0aeb5656e469a4a0635
Bhagyashree-Srinivasan <bhagyashree6599@gmail.com>
2025-08-04 23:03:10 +0530

    config added

---
+++ utils/config_loader.py
@@ -0,0 +1,15 @@
+import yaml
+
+def load_config(config_path: str = "config/config.yaml") -> dict:
+    """
+    Load configuration from a YAML file.
+
+    Args:
+        config_path (str): The path to the configuration file.
+
+    Returns:
+        dict: The configuration data.
+    """
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
